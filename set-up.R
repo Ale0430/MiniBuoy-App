86ba3a1dade2d0809cbd6917443336075d1f3edd
marie <mcwimm@bitbucket.org>
2022-09-08 08:38:37 +0200

    clean + comment setup file

--- set-up.R
+++ set-up.R
@@ -1,29 +1,40 @@
 
 #### LOAD PACKAGES ####
 
+# Basic shiny package
 if(!require("shiny")) install.packages("shiny")  
+# Package for shiny dashboard layout
 if(!require("shinydashboard")) install.packages("shinydashboard")  
+# Package to add loading animations when output is recalculating 
 if(!require("shinycssloaders")) install.packages("shinycssloaders") 
+# Package to manage to browse local folder system
 if(!require("shinyFiles")) install.packages("shinyFiles") 
+# Package to use markdown
 if(!require("markdown")) install.packages("markdown")  
-if(!require("DT")) install.packages("DT")  
-if(!require("data.table")) install.packages("data.table")  # fread function to load csv files faster
-if(!require("fasttime")) install.packages("fasttime")  # fastPOSIXct  function to increase of datetime column creation
-if(!require("lubridate")) install.packages("lubridate")  # as_date  function to increase of time column creation
-if(!require("zoo")) install.packages("zoo") # rollapplyfunction  #from the first Mini Buoy app (needed?)
+# Package collection to simplify data processing
 if(!require("tidyverse")) install.packages("tidyverse") 
-if(!require("ggpubr")) install.packages("ggpubr") # draw regression line in ggplot
-if(!require("scales")) install.packages("scales") # modify datetime format in ggpplot
-if(!require("kernlab")) install.packages("kernlab")   #from Cai's code
+# Package to render pretty tables -> renderDataTable()
+if(!require("DT")) install.packages("DT")  
+# Package to speed up file upload -> fread()
+if(!require("data.table")) install.packages("data.table")
+# Package to deal with dates and times -> as_date(), ceiling_date()
+if(!require("lubridate")) install.packages("lubridate")
+# Package to deal with time serien -> rollmean()
+if(!require("zoo")) install.packages("zoo") 
+# ???
 if(!require("caTools")) install.packages("caTools")  #from Cai's code
-#if(!require("fontawesome")) install.packages("fontawesome")  #dashboard menu icons (not really needed#)
 
+# # Check if the following packages are required
+# # Package to speed up processing of date formats -> fastPOSIXct
+# if(!require("fasttime")) install.packages("fasttime") 
+# if(!require("ggpubr")) install.packages("ggpubr") # draw regression line in ggplot
+# if(!require("scales")) install.packages("scales") # modify datetime format in ggpplot
+# #if(!require("fontawesome")) install.packages("fontawesome")  #dashboard menu icons (not really needed#)
+# ???
+# if(!require("kernlab")) install.packages("kernlab")   #from Cai's code
 
 
 #### LOAD APP FUNCTIONS ####
 path= "./R_scripts/functions/"
 modelFunctions = list.files(path)
 lapply(modelFunctions, function(x) source(paste(path, x, sep ="")))
-
-path
-list.files(path)
