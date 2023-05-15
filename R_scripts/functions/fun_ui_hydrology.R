d997dc6c0f219eccf79ae4f43e3890b8205e7f87
marie <mc.wimmler@gmx.de>
2023-05-15 16:43:23 +0200

    [hydro] update hydro reference page

--- R_scripts/functions/fun_ui_hydrology.R
+++ R_scripts/functions/fun_ui_hydrology.R
@@ -27,15 +27,9 @@ hydTargetOutput = function(){
       box(title = "Plots",
           width = "100%",
           collapsible = T, status = "success",
-          hyd.target.box.figures())#,
-      
-      # box(title = "Disclaimer",
-      #     width = "100%",
-      #     collapsible = T, status = "info",
-      #     p("Load md. file containing disclaimer. @Cai: please prepare such file and name it 'disclaimer.md' (store it it man)."))
+          hyd.target.box.figures())
     )
   )
-  
 }
 
 
@@ -129,37 +123,72 @@ hyd.target.box.figures = function(){
 hydReferenceOutput = function(){
   return(
     list(
+      box(title = "Settings",
+          width = "100%",
+          collapsible = T,
+          collapsed = T,
+          status = "success",
+          hyd.reference.box.settings()),
       fluidRow(
-        box(title = "Results",
+        box(title = "Summary",
                    width = 3, height = "95%",
                    collapsible = T, status = "success",
                    hyd.reference.box.text()),
         
-        box(title = "Summary table",
+        box(title = "Table",
                    width = 9, height = "95%",
                    collapsible = T, status = "success",
                    hyd.reference.box.table())
       ),
       
-      box(title = "Select a plot",
+      box(title = "Plots",
           width = "100%",
           collapsible = T, status = "success",
-          hyd.reference.box.figures())#,
-      
-      # box(title = "Disclaimer",
-      #     width = "100%",
-      #     collapsible = T, status = "info",
-      #     p("Load md. file containing disclaimer. @Cai: please prepare such file and name it 'disclaimer.md' (store it it man)."))
+          hyd.reference.box.figures())
     )
   )
   
 }
 
+hyd.reference.box.settings = function(){
+  return(
+    list(
+      splitLayout(
+        cellWidths = 300,
+        checkboxInput( "hydro_set_cust_reference", "Custom settings", F),
+        uiOutput("hydro.window.reference.show")
+      ),
+    conditionalPanel(
+      condition = "input.hydro_set_cust_reference == true",
+      # Default values: gaps = 20, full = 20, part = 90, tilt = 75
+      splitLayout(
+        numericInput(inputId = "hydro.set.gaps.reference",
+                     label = HTML("<abbr title='Minimum gap in an inundation event to be closed, where points were misclassified as non-inundated (minutes)'>Minimum gap</abbr>"),
+                     value = 20),
+        numericInput(inputId = "hydro.set.part.reference",
+                     label = HTML("<abbr title='Time window to search for partially inundated cases at the start and end of inundation events (minutes).'>Partial inundation window</abbr>"),
+                     value = 90)
+      ),
+      splitLayout(
+        numericInput(inputId = "hydro.set.full.reference",
+                     label = HTML("<abbr title='Minimum duration of a fully inundated event, otherwise event is reclassified as partially inundated (minutes)'>Minimum full inundation</abbr>"),
+                     value = 20),
+        numericInput(inputId = "hydro.set.tilt.reference",
+                     label = HTML("<abbr title='Minimum tilt to classify an event as fully inundated, otherwise event is reclassified as partially inundated (degrees)'>Tilt full inundation</abbr>"),
+                     value = 75)
+      ),
+      splitLayout(
+        actButton("hydro.set.apply.reference", "Apply custom settings", "update"),
+        actButton("hydro.set.reset.reference", "Reset custom settings", "grey")
+      )
+      )
+    )
+  )
+}
 
 hyd.reference.box.text = function(){
   return(
     list(
-      uiOutput("hydro.window.reference.show"),
       output.html("hydro.text.reference")
     )
   )
@@ -177,17 +206,17 @@ hyd.reference.box.figures = function(){
   return(list(
     tabsetPanel(
       tabPanel("Daily inundation", br(),
-               output.figure("fig.inundation.reference"),
+               plotlyOutput("fig.inundation.reference"),
                actButton("save.fig.inundation.reference",
                          "Save figure", 
                          "saveFigure")),
       tabPanel("Current velocity", br(),
-               output.figure("fig.velocity.reference"),
+               plotlyOutput("fig.velocity.reference"),
                actButton("save.fig.velocity.reference",
                          "Save figure", 
                          "saveFigure")),
       tabPanel("Wave orbital velocity", br(),
-               output.figure("fig.wave.velocity.reference"),
+               plotlyOutput("fig.wave.velocity.reference"),
                actButton("save.fig.wave.velocity.reference",
                          "Save figure", 
                          "saveFigure"))
